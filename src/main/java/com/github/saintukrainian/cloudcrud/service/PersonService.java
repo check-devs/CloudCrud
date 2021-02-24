0e1ae18e6618b03beac7cccf488ab5ff77403f3b
Denys Matsenko <idanchik47@gmail.com>
2021-02-24 07:13:44 -0800

    completablefuture added instead of future + redundant logs removed

--- src/main/java/com/github/saintukrainian/cloudcrud/service/PersonService.java
+++ src/main/java/com/github/saintukrainian/cloudcrud/service/PersonService.java
@@ -48,9 +48,6 @@ public class PersonService {
     @Value("${sql.latest-person}")
     private String LATEST_PERSON_SQL;
 
-
-    private ExecutorService executorService = Executors.newFixedThreadPool(2);
-
     private final static Logger logger;
 
     static {
@@ -71,6 +68,7 @@ public class PersonService {
         logger.info("Finding person with id=" + id);
         long time = System.currentTimeMillis();
         Optional<Person> person = personRepository.findById(id);
+
         if (person.isPresent()) {
             logger.info("Person found. Time taken: " + (System.currentTimeMillis() - time) + " milliseconds");
             return person.get();
@@ -81,41 +79,35 @@ public class PersonService {
     }
 
     public PersonDetails getPersonDetailsById(int id) {
-        logger.info("Finding person details by id=" + id);
         Optional<PersonDetails> personDetails = peronsDetailsRepository.findById(id);
+
         if (personDetails.isPresent()) {
             return personDetails.get();
         } else {
-            logger.warning("Person details were not found with id=" + id);
             throw new PersonDetailsNotFoundException();
         }
     }
 
     public Iterable<Person> findAllPersons() {
-        logger.info("Finding all persons");
         return personRepository.findAll();
     }
 
     public void savePerson(Person person) throws BadRequestException {
-        logger.info("Saving person with firstName=" + person.getFirstName());
         int newUserId;
+
         if (person.getId() == 0) {
             newUserId = this.getLatestPersonEntry().getId() + 1;
             person.setId(newUserId);
         } else {
-            logger.warning("Bad request for saving person. ID was present.");
             throw new BadRequestException();
         }
         personRepository.save(person);
-        logger.info("Person saved with new id=" + newUserId);
     }
 
     public void updatePerson(int userId, Person person) {
-        logger.info("Updating person with id=" + userId);
         if (this.checkIfPersonExistsById(userId)) {
             person.setId(userId);
             personRepository.save(person);
-            logger.info("Person with id=" + userId + " was successfully updated!");
         } else {
             logPersonWasNotFoundWithId(userId);
             throw new PersonNotFoundException();
@@ -124,16 +116,14 @@ public class PersonService {
 
     public void savePersonDetails(PersonDetails personDetails) throws BadRequestException, PersonNotFoundException {
         if (personDetails.getDetailsId() != 0) {
-            logger.warning("Bad request for saving person details. Details ID was present!");
             throw new BadRequestException();
         }
 
         int userId = personDetails.getUserId();
-        logger.info("Adding person details for person with id=" + userId);
+
         if (this.checkIfPersonExistsById(userId)) {
             personDetails.setDetailsId(userId);
             peronsDetailsRepository.save(personDetails);
-            logger.info("Person details were successfully set for person with id=" + userId);
         } else {
             logPersonWasNotFoundWithId(userId);
             throw new PersonNotFoundException();
@@ -141,12 +131,10 @@ public class PersonService {
     }
 
     public void updatePersonDetails(int id, PersonDetails personDetails) {
-        logger.info("Updating person details for person with id=" + personDetails.getUserId());
         if (this.checkIfPersonDetailsExistById(id)) {
             personDetails.setUserId(id);
             personDetails.setDetailsId(id);
             peronsDetailsRepository.save(personDetails);
-            logger.info("Person details were successfully updated for person with id=" + id);
         } else {
             logPersonWasNotFoundWithId(id);
             throw new PersonNotFoundException();
@@ -154,10 +142,8 @@ public class PersonService {
     }
 
     public void deletePersonById(int id) {
-        logger.info("Deleting person with id=" + id);
         try {
             personRepository.deleteById(id);
-            logger.info("Person was deleted with id=" + id);
         } catch (IllegalArgumentException e) {
             logPersonFoundWithId(id);
             throw new PersonNotFoundException();
@@ -165,28 +151,21 @@ public class PersonService {
     }
 
     public void deletePersonDetailsById(int id) {
-        logger.info("Deleting person details for person with id=" + id);
         try {
             peronsDetailsRepository.deleteById(id);
-            logger.info("Person details were successfully deleted for person with id=" + id);
         } catch (IllegalArgumentException e) {
-            logger.warning("Can't delete person details. Person was not found with id=" + id);
             throw new PersonDetailsNotFoundException();
         }
     }
 
     public void suppressedDeletePersonDetailsById(int id) {
-        logger.info("Deleting person details for person with id=" + id);
         try {
             peronsDetailsRepository.deleteById(id);
-            logger.info("Person details were successfully deleted for person with id=" + id);
         } catch (IllegalArgumentException e) {
-            logger.warning("Can't delete person details. Person was not found with id=" + id);
         }
     }
 
     public List<Person> getAllPersonsByFirstName(String firstName) {
-        logger.info("Finding all person by firstName=" + firstName);
         Statement statement = Statement.newBuilder(PERSONS_BY_FIRST_NAME_SQL)
                 .bind("firstName")
                 .to(firstName)
@@ -196,20 +175,19 @@ public class PersonService {
     }
 
     public List<PersonWithDetails> getAllPersonsWithDetails() {
-        logger.info("Finding all persons with details");
         SpannerQueryOptions queryOptions = new SpannerQueryOptions();
         return spannerTemplate.query(PersonWithDetails.class, Statement.of(PERSONS_WITH_DETAILS_SQL),
                 queryOptions);
     }
 
     public PersonWithDetails getPersonWithDetailsById(int id) throws IllegalArgumentException {
-        logger.info("Finding person with details by id=" + id);
         SpannerQueryOptions queryOptions = new SpannerQueryOptions();
         Statement statement = Statement.newBuilder(PERSON_WITH_DETAILS_SQL).bind("id")
                 .to(id)
                 .build();
         List<PersonWithDetails> personWithDetails =
                 spannerTemplate.query(PersonWithDetails.class, statement, queryOptions);
+
         if (CollectionUtils.isNotEmpty(personWithDetails)) {
             logPersonFoundWithId(id);
             return personWithDetails.get(0);
@@ -220,17 +198,18 @@ public class PersonService {
     }
 
 
-    public PersonWithPosts getPersonWithPostsById(int id) throws InterruptedException,
-            TimeoutException, ExecutionException {
-        logger.info("Finding all posts for person with id=" + id);
-        long time = System.currentTimeMillis();
-        if (personRepository.existsById(id)) {
-            Future<Person> person = getPersonByIdAsync(id);
-            Future<List<Post>> posts = getPostsByPersonIdAsync(id);
+    public PersonWithPosts getPersonWithPostsById(int id) throws InterruptedException, ExecutionException {
 
+        long time = System.currentTimeMillis();
+
+        if (personRepository.existsById(id)) {
+            CompletableFuture<Person> person = CompletableFuture.supplyAsync(() -> getPersonById(id));
+            CompletableFuture<List<Post>> posts = CompletableFuture.supplyAsync(() -> postService.getPostsByUserId(id));
             PersonWithPosts personWithPosts = new PersonWithPosts();
-            personWithPosts.setFieldsWithPersonInfo(person.get(1000, TimeUnit.MILLISECONDS));
-            personWithPosts.setPosts(posts.get(1000, TimeUnit.MILLISECONDS));
+
+            personWithPosts.setFieldsWithPersonInfo(person.get());
+            personWithPosts.setPosts(posts.get());
+
             logger.info("Person with posts method completed in " +
                     (System.currentTimeMillis() - time) + " milliseconds");
             return personWithPosts;
@@ -242,27 +221,25 @@ public class PersonService {
     }
 
     public Person getLatestPersonEntry() throws IndexOutOfBoundsException {
-        logger.info("Finding latest person entry");
         SpannerQueryOptions queryOptions = new SpannerQueryOptions();
         List<Person> person = spannerTemplate.query(Person.class, Statement.of(LATEST_PERSON_SQL), queryOptions);
+
         if (CollectionUtils.isNotEmpty(person)) {
             logPersonFound();
             return person.get(0);
         } else {
-            logger.warning("Hmm, table seems to be empty.");
             throw new PersonNotFoundException();
         }
     }
 
     public PersonDetails getLatestPersonDetailsEntry() throws IndexOutOfBoundsException {
-        logger.info("Finding latest person details entry");
         SpannerQueryOptions queryOptions = new SpannerQueryOptions();
         List<PersonDetails> personDetails =
                 spannerTemplate.query(PersonDetails.class, Statement.of(LATEST_PERSON_DETAILS_SQL), queryOptions);
+
         if (CollectionUtils.isNotEmpty(personDetails)) {
             return personDetails.get(0);
         } else {
-            logger.warning("Hmm, table seems to be empty.");
             throw new PersonDetailsNotFoundException();
         }
     }
@@ -286,19 +263,4 @@ public class PersonService {
     private void logPersonWasNotFoundWithId(int personId) {
         logger.warning("Person was not found with id=" + personId);
     }
-
-    private void logPersonWasNotFound() {
-        logger.warning("Person was not found");
-    }
-
-    public Future<Person> getPersonByIdAsync(int id) {
-        System.out.println("In findPersonByIdAsync");
-        return executorService.submit(() -> getPersonById(id));
-    }
-
-    public Future<List<Post>> getPostsByPersonIdAsync(int id) {
-        System.out.println("In  findPostsByPersonIdAsync");
-        return executorService.submit(() -> postService.getPostsByUserId(id));
-    }
-
 }
