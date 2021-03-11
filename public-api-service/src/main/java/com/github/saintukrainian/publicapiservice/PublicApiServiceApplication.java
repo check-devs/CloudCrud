0e78543a099a189d11ddb3ae3f6603367e47e09d
Denys Matsenko <idanchik47@gmail.com>
2021-03-11 02:57:51 -0800

    saving changes for public api service

--- public-api-service/src/main/java/com/github/saintukrainian/publicapiservice/PublicApiServiceApplication.java
+++ public-api-service/src/main/java/com/github/saintukrainian/publicapiservice/PublicApiServiceApplication.java
@@ -1,11 +1,15 @@
 package com.github.saintukrainian.publicapiservice;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Slf4j
+@RequiredArgsConstructor
 public class PublicApiServiceApplication {
 
 	public static void main(String[] args) {
--- public-api-service/src/main/java/com/github/saintukrainian/publicapiservice/controller/PostController.java
+++ public-api-service/src/main/java/com/github/saintukrainian/publicapiservice/controller/PostController.java
@@ -3,6 +3,7 @@ package com.github.saintukrainian.publicapiservice.controller;
 import com.github.saintukrainian.publicapiservice.entities.Post;
 import com.github.saintukrainian.publicapiservice.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,37 +14,34 @@ import java.util.List;
 /**
  * @author Denys Matsenko
  * @version 1.0.0
- * <p>
- * The {@code PostController} class handles GET requests for {@code Post} entity.
+ *     <p>The {@code PostController} class handles GET requests for {@code Post} entity.
  */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
 public class PostController {
 
-    /**
-     * The {@code PostService} reference
-     */
-    private final PostService postService;
+  /** The {@code PostService} reference */
+  private final PostService postService;
 
-    /**
-     * GET method for getting posts by person id
-     *
-     * @param id person id
-     * @return list of person related posts
-     */
-    @GetMapping("/{id}")
-    public List<Post> getPostsByUserId(@PathVariable int id) {
-        return postService.getPostsByUserId(id);
-    }
+  /**
+   * GET method for getting posts by person id
+   *
+   * @param id person id
+   * @return list of person related posts
+   */
+  @GetMapping("/{id}")
+  public List<Post> getPostsByUserId(@PathVariable int id) {
+    return postService.getPostsByUserId(id);
+  }
 
-    /**
-     * GET method for getting posts
-     *
-     * @return list of posts
-     */
-    @GetMapping("/")
-    public List<Post> getPosts() {
-        return postService.getAllPosts();
-    }
+  /**
+   * GET method for getting posts
+   *
+   * @return list of posts
+   */
+  @GetMapping("/")
+  public List<Post> getPosts() {
+    return postService.getAllPosts();
+  }
 }
