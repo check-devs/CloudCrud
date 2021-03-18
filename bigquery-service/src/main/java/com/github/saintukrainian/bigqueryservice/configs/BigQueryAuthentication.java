0b5a86b4abb655e5595756e399cad4a2f954997d
Denys Matsenko <idanchik47@gmail.com>
2021-03-18 05:24:12 -0700

    changed back to spring auto configuration for gcp credentials

--- bigquery-service/src/main/java/com/github/saintukrainian/bigqueryservice/configs/BigQueryAuthentication.java
+++ bigquery-service/src/main/java/com/github/saintukrainian/bigqueryservice/configs/BigQueryAuthentication.java
@@ -1,14 +1,8 @@
 package com.github.saintukrainian.bigqueryservice.configs;
 
-import com.google.auth.Credentials;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 @Slf4j
 public class BigQueryAuthentication {
@@ -21,19 +15,8 @@ public class BigQueryAuthentication {
 
   public static BigQuery getBigQuery() {
     if (bigQuery == null) {
-      try {
-        bigQuery =
-            BigQueryOptions.newBuilder()
-                .setCredentials(
-                    GoogleCredentials.fromStream(
-                        new FileInputStream(
-                            "src/main/resources/cloud-crud-304511-1613e3453d69.json")))
-                .build()
-                .getService();
-      } catch (IOException e) {
-        log.error("Credentials not found!");
-        e.printStackTrace();
-      }
+      bigQuery =
+          BigQueryOptions.getDefaultInstance().getService();
     }
     return bigQuery;
   }
