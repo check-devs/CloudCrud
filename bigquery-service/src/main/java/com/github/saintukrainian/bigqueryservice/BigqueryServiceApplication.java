b756ba28c58e9ee3edf921de949496817e88f4c1
Denys Matsenko <idanchik47@gmail.com>
2021-03-19 03:00:17 -0700

    auto config fix by setting env GOOGLE_APPLICATION_CREDENTIALS=src/main/resources/application.properties

--- bigquery-service/src/main/java/com/github/saintukrainian/bigqueryservice/BigqueryServiceApplication.java
+++ bigquery-service/src/main/java/com/github/saintukrainian/bigqueryservice/BigqueryServiceApplication.java
@@ -5,16 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-import javax.annotation.PostConstruct;
-
 @Slf4j
 @SpringBootApplication
 @EnableEurekaClient
 public class BigqueryServiceApplication {
-
-  @PostConstruct
-  public void init() {
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(BigqueryServiceApplication.class, args);
--- bigquery-service/src/main/java/com/github/saintukrainian/bigqueryservice/configs/BigQueryAuthentication.java
+++ bigquery-service/src/main/java/com/github/saintukrainian/bigqueryservice/configs/BigQueryAuthentication.java
@@ -1,8 +1,11 @@
 package com.github.saintukrainian.bigqueryservice.configs;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
 import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
 
 @Slf4j
 public class BigQueryAuthentication {
@@ -16,8 +19,11 @@ public class BigQueryAuthentication {
   // in progress
   public static BigQuery getBigQuery() {
     if (bigQuery == null) {
+      log.info("Instantiating bigquery...");
+      log.info("project id from bigquery options: {}", BigQueryOptions.getDefaultProjectId());
       bigQuery =
-          BigQueryOptions.getDefaultInstance().getService();
+          BigQueryOptions.getDefaultInstance()
+              .getService();
     }
     return bigQuery;
   }
