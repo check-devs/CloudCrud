67e474f1f854626479ac58309f0ec22de4348ee8
Denys Matsenko <idanchik47@gmail.com>
2021-03-19 03:33:08 -0700

    redudant annotation has been removed

--- bigquery-service/src/main/java/com/github/saintukrainian/bigqueryservice/BigqueryServiceApplication.java
+++ bigquery-service/src/main/java/com/github/saintukrainian/bigqueryservice/BigqueryServiceApplication.java
@@ -1,11 +1,9 @@
 package com.github.saintukrainian.bigqueryservice;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@Slf4j
 @SpringBootApplication
 @EnableEurekaClient
 public class BigqueryServiceApplication {
